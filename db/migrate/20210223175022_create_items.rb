dc8ab986d5e258dd81efbd0222faec8a1aefbce7
ben20262 <ben20262@aim.com>
2021-02-23 12:51:01 -0500

    Created Item resource

---
+++ db/migrate/20210223175022_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.interger :quantity
+      t.datetime :arrived
+      t.datetime :sold
+
+      t.timestamps
+    end
+  end
+end
